10bc51ab11e590e25e09ebd4c9cef6021f0a88d6
Martin Linxfeld <martin.linfeld@foggykitchen.com>
2020-02-07 13:55:59 +0100

    fixes on LESSON9 v.1

--- LESSON9_vcn_remote_peering/subnet4.tf
+++ LESSON9_vcn_remote_peering/subnet4.tf
@@ -6,7 +6,7 @@ resource "oci_core_subnet" "FoggyKitchenDBSubnet" {
   prohibit_public_ip_on_vnic = true
   compartment_id = oci_identity_compartment.FoggyKitchenCompartment.id
   vcn_id = oci_core_virtual_network.FoggyKitchenVCN.id
-  route_table_id = oci_core_route_table.FoggyKitchenLPG1RouteTable.id
+  route_table_id = oci_core_route_table.FoggyKitchenRouteTableViaNAT.id
   dhcp_options_id = oci_core_dhcp_options.FoggyKitchenDhcpOptions1.id
   security_list_ids = [oci_core_security_list.FoggyKitchenSSHSecurityList.id,oci_core_security_list.FoggyKitchenSQLNetSecurityList.id]
 }
