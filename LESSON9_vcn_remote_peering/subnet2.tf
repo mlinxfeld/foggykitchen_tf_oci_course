87bb98f78c408e74eb7191fc0b4a1ae9c586c489
Martin Linxfeld <martin.linfeld@foggykitchen.com>
2020-02-10 12:02:50 +0100

    Fixes on LESSON9_vcn_remote_peering 2

--- LESSON9_vcn_remote_peering/subnet2.tf
+++ LESSON9_vcn_remote_peering/subnet2.tf
@@ -5,7 +5,7 @@ resource "oci_core_subnet" "FoggyKitchenWebSubnet" {
   dns_label = "FoggyKitchenN2"
   compartment_id = oci_identity_compartment.FoggyKitchenCompartment.id
   vcn_id = oci_core_virtual_network.FoggyKitchenVCN.id
-  route_table_id = oci_core_route_table.FoggyKitchenRouteTableViaNAT.id
+  route_table_id = oci_core_route_table.FoggyKitchenRouteTableViaNATandDRG1.id
   dhcp_options_id = oci_core_dhcp_options.FoggyKitchenDhcpOptions1.id
   security_list_ids = [oci_core_security_list.FoggyKitchenWebSecurityList.id,oci_core_security_list.FoggyKitchenSSHSecurityList.id]
 }
