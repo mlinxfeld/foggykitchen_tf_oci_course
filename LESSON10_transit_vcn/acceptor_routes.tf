18bc720bb345e4243aaa4efd89d4556ae75e431c
Martin Linxfeld <martin.linfeld@foggykitchen.com>
2020-02-13 22:48:14 +0100

    LESSON10_transit_vcn v.0.15

--- LESSON10_transit_vcn/acceptor_routes.tf
+++ LESSON10_transit_vcn/acceptor_routes.tf
@@ -42,3 +42,49 @@ resource "oci_core_route_table" "FoggyKitchenDRG2RouteTable" {
   }
 
 }
+
+resource "oci_core_route_table" "FoggyKitchenSPOKELPG1RouteTable" {
+  provider = oci.acceptor
+  compartment_id = oci_identity_compartment.ExternalCompartment.id 
+  vcn_id = oci_core_virtual_network.FoggyKitchenSpokeVCN3.id
+  display_name = "FoggyKitchenSPOKELPG1RouteTable"
+  
+  route_rules {
+    cidr_block = var.VCN-CIDR2 # to HUBVCN
+    network_entity_id = oci_core_local_peering_gateway.FoggyKitchenSPOKELPG1.id
+  }
+
+  route_rules {
+    cidr_block = var.VCN-CIDR # to FRA infra
+    network_entity_id = oci_core_local_peering_gateway.FoggyKitchenSPOKELPG1.id
+  }
+
+  route_rules {
+    cidr_block = "0.0.0.0/0" # to Internet
+    network_entity_id = oci_core_internet_gateway.ExternalInternetGateway1.id
+  }
+
+}
+
+resource "oci_core_route_table" "FoggyKitchenSPOKELPG2RouteTable" {
+  provider = oci.acceptor
+  compartment_id = oci_identity_compartment.ExternalCompartment.id 
+  vcn_id = oci_core_virtual_network.FoggyKitchenSpokeVCN4.id
+  display_name = "FoggyKitchenSPOKELPG2RouteTable"
+  
+  route_rules {
+    cidr_block = var.VCN-CIDR2 # to HUBVCN
+    network_entity_id = oci_core_local_peering_gateway.FoggyKitchenSPOKELPG2.id
+  }
+
+  route_rules {
+    cidr_block = var.VCN-CIDR # to FRA infra
+    network_entity_id = oci_core_local_peering_gateway.FoggyKitchenSPOKELPG2.id
+  }
+
+  route_rules {
+    cidr_block = "0.0.0.0/0" # to Internet
+    network_entity_id = oci_core_internet_gateway.ExternalInternetGateway2.id
+  }
+
+}
