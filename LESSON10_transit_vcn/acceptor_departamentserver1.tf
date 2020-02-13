6ad43c0d5d66928a844c96419aeefd853661739b
Martin Linxfeld <martin.linfeld@foggykitchen.com>
2020-02-13 22:52:40 +0100

    LESSON10_transit_vcn v.0.16

--- LESSON10_transit_vcn/acceptor_departamentserver1.tf
+++ LESSON10_transit_vcn/acceptor_departamentserver1.tf
@@ -14,7 +14,7 @@ resource "oci_core_instance" "FoggyKitchenDepartamentServer1" {
   }
   create_vnic_details {
      subnet_id = oci_core_subnet.FoggyKitchenSpokeSubnet1.id
-     assign_public_ip = false 
+ #    assign_public_ip = false 
   }
 }
 
