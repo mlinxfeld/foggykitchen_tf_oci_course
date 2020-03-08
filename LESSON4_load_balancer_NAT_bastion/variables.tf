aac6fabac25dc03d1b7277a4adb70e96c77ab879
Martin Linxfeld <martin.linxfeld@foggykitchen.com>
2020-03-08 11:18:27 +0100

    OSImage + AD for LESSON4

--- LESSON4_load_balancer_NAT_bastion/variables.tf
+++ LESSON4_load_balancer_NAT_bastion/variables.tf
@@ -15,18 +15,23 @@ variable "VCNname" {
   default = "FoggyKitchenVCN"
 }
 
-variable "ADs" {
-  default = ["unja:EU-FRANKFURT-1-AD-1", "unja:EU-FRANKFURT-1-AD-2", "unja:EU-FRANKFURT-1-AD-3"]
-}
+#variable "ADs" {
+#  default = ["unja:EU-FRANKFURT-1-AD-1", "unja:EU-FRANKFURT-1-AD-2", "unja:EU-FRANKFURT-1-AD-3"]
+#}
 
 variable "Shapes" {
  default = ["VM.Standard.E2.1","VM.Standard.E2.1.Micro","VM.Standard2.1","VM.Standard.E2.1","VM.Standard.E2.2"]
 }
 
-variable "Images" {
- # Oracle-Linux-7.7-2019.11.12-0 in Frankfurt
- default = ["ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa3bu75jht762mfvwroa2gdck6boqwyktztyu5dfhftcycucyp63ma"]
+variable "OsImage" {
+  # Oracle-Linux-7.7-2020.02.21-0 in Frankfurt
+  default = "Oracle-Linux-7.7-2020.02.21-0"
 }
+
+#variable "Images" {
+ # Oracle-Linux-7.7-2019.11.12-0 in Frankfurt
+# default = ["ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa3bu75jht762mfvwroa2gdck6boqwyktztyu5dfhftcycucyp63ma"]
+#}
 
 variable "webservice_ports" {
   default = [80,443]
