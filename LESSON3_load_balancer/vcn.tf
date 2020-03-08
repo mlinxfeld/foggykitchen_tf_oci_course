2b541b4e54e02d1dd3f120d549797eb24a4186fa
Martin Linxfeld <martin.linxfeld@foggykitchen.com>
2020-03-08 11:12:08 +0100

    OSImage + AD for LESSON3

--- LESSON3_load_balancer/vcn.tf
+++ LESSON3_load_balancer/vcn.tf
@@ -4,3 +4,15 @@ resource "oci_core_virtual_network" "FoggyKitchenVCN" {
   compartment_id = oci_identity_compartment.FoggyKitchenCompartment.id
   display_name = "FoggyKitchenVCN"
 }
+
+# Gets a list of Availability Domains
+data "oci_identity_availability_domains" "ADs" {
+  compartment_id = var.tenancy_ocid
+}
+
+# Gets the Id of a specific OS Images
+data "oci_core_images" "OSImageLocal" {
+  #Required
+  compartment_id = var.compartment_ocid
+  display_name   = var.OsImage
+}
