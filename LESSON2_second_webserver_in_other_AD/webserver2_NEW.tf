d3e606aaed7fb9ed70f34a83fc539fb463e3fe37
Martin Linxfeld <martin.linxfeld@foggykitchen.com>
2020-03-08 11:05:13 +0100

    OSImage + AD for LESSON2

--- LESSON2_second_webserver_in_other_AD/webserver2_NEW.tf
+++ LESSON2_second_webserver_in_other_AD/webserver2_NEW.tf
@@ -1,12 +1,12 @@
 resource "oci_core_instance" "FoggyKitchenWebserver2" {
-  availability_domain = var.ADs[2]
+  availability_domain = lookup(data.oci_identity_availability_domains.ADs.availability_domains[2], "name")
   compartment_id = oci_identity_compartment.FoggyKitchenCompartment.id
   display_name = "FoggyKitchenWebServer2"
   shape = var.Shapes[0]
   subnet_id = oci_core_subnet.FoggyKitchenWebSubnet.id
   source_details {
     source_type = "image"
-    source_id   = var.Images[0]
+    source_id   = lookup(data.oci_core_images.OSImageLocal.images[0], "id")
   }
   metadata = {
       ssh_authorized_keys = file(var.public_key_oci)
@@ -18,7 +18,7 @@ resource "oci_core_instance" "FoggyKitchenWebserver2" {
 }
 
 data "oci_core_vnic_attachments" "FoggyKitchenWebserver2_VNIC1_attach" {
-  availability_domain = var.ADs[2]
+  availability_domain = lookup(data.oci_identity_availability_domains.ADs.availability_domains[2], "name")
   compartment_id = oci_identity_compartment.FoggyKitchenCompartment.id
   instance_id = oci_core_instance.FoggyKitchenWebserver2.id
 }
