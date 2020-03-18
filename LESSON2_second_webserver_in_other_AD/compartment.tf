d5ea64018b89615d4aaccd852d7dd64e8e43543f
Martin Linxfeld <martin.linxfeld@foggykitchen.com>
2020-03-18 18:50:40 +0100

    Subcompartment support for all lessons v2

--- LESSON2_second_webserver_in_other_AD/compartment.tf
+++ LESSON2_second_webserver_in_other_AD/compartment.tf
@@ -1,6 +1,6 @@
 resource "oci_identity_compartment" "FoggyKitchenCompartment" {
   name = "FoggyKitchenCompartment"
   description = "FoggyKitchen Compartment"
-  compartment = var.compartment_ocid
+  compartment_id = var.compartment_ocid
 }
 
