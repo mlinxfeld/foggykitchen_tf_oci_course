8b25d88354a2a63f4181f731d780f8b21cfe3ffd
Martin Linxfeld <martin.linxfeld@foggykitchen.com>
2020-03-18 18:43:11 +0100

    Subcompartment support for all lessons

--- LESSON9_vcn_remote_peering/compartment.tf
+++ LESSON9_vcn_remote_peering/compartment.tf
@@ -1,9 +1,11 @@
 resource "oci_identity_compartment" "FoggyKitchenCompartment" {
   name = "FoggyKitchenCompartment"
   description = "FoggyKitchenCompartment"
+  compartment = var.compartment_ocid
 }
 
 resource "oci_identity_compartment" "ExternalCompartment" {
   name = "ExternalCompartment"
   description = "External Compartment"
+  compartment = var.compartment_ocid
 }
