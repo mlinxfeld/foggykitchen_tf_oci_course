a8793a769879b0e13ab84a1a3a404f26960b4c1c
mlinxfeld <martin.linxfeld@foggykitchen.com>
2021-01-26 11:50:05 +0100

    Lesson 1 - Support for ORM Deploy Button v8

--- LESSON1_single_webserver/provider.tf
+++ LESSON1_single_webserver/provider.tf
@@ -1,9 +1,9 @@
 provider "oci" {
   version          = ">= 4.3.0" 
   tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+#  user_ocid        = var.user_ocid
+#  fingerprint      = var.fingerprint
+#  private_key_path = var.private_key_path
   region           = var.region
 }
 
--- LESSON1_single_webserver/variables.tf
+++ LESSON1_single_webserver/variables.tf
@@ -1,7 +1,7 @@
 variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
+#variable "user_ocid" {}
+#variable "fingerprint" {}
+#variable "private_key_path" {}
 variable "compartment_ocid" {}
 variable "region" {}
 variable "availablity_domain_name" {}
