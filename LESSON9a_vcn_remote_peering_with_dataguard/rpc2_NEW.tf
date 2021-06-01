9c42ba7fbaa5f1d05e1ceb1f1fd33618bc808ce6
mlinxfeld <martin.linxfeld@foggykitchen.com>
2021-06-01 15:46:34 +0200

    Updated LESSON9a

--- LESSON9a_vcn_remote_peering_with_dataguard/rpc2_NEW.tf
+++ LESSON9a_vcn_remote_peering_with_dataguard/rpc2_NEW.tf
@@ -1,5 +1,6 @@
 resource "oci_core_drg" "FoggyKitchenDRG2" {
   depends_on     = [oci_identity_policy.FoggyKitchenAcceptorPolicy, oci_identity_user_group_membership.FoggyKitchenAcceptorUserGroupMembership]
+#  depends_on     = [oci_identity_policy.FoggyKitchenAcceptorPolicy]
   provider       = oci.acceptor
   display_name   = "FoggyKitchenDRG2"
   compartment_id = oci_identity_compartment.ExternalCompartment.id
@@ -7,6 +8,7 @@ resource "oci_core_drg" "FoggyKitchenDRG2" {
 
 resource "oci_core_drg_attachment" "FoggyKitchenDRG2Attachment" {
   depends_on = [oci_identity_policy.FoggyKitchenAcceptorPolicy, oci_identity_user_group_membership.FoggyKitchenAcceptorUserGroupMembership]
+#  depends_on     = [oci_identity_policy.FoggyKitchenAcceptorPolicy]
   provider   = oci.acceptor
   drg_id     = oci_core_drg.FoggyKitchenDRG2.id
   vcn_id     = oci_core_virtual_network.FoggyKitchenVCN2.id
@@ -14,6 +16,7 @@ resource "oci_core_drg_attachment" "FoggyKitchenDRG2Attachment" {
 
 resource "oci_core_remote_peering_connection" "FoggyKitchenRPC2" {
   depends_on     = [oci_identity_policy.FoggyKitchenAcceptorPolicy, oci_identity_user_group_membership.FoggyKitchenAcceptorUserGroupMembership]
+#  depends_on     = [oci_identity_policy.FoggyKitchenAcceptorPolicy]
   provider       = oci.acceptor
   compartment_id = oci_identity_compartment.ExternalCompartment.id
   drg_id         = oci_core_drg.FoggyKitchenDRG2.id
