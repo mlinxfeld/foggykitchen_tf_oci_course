239a96f78737410edf92ada81c0a261f706121f2
mlinxfeld <martin.linxfeld@foggykitchen.com>
2021-07-07 18:37:40 +0200

    Refreshed pictures + sensitive outputs

--- LESSON4_load_balancer_NAT_bastion/output.tf
+++ LESSON4_load_balancer_NAT_bastion/output.tf
@@ -1,5 +1,5 @@
 output "FoggyKitchenBastionServer_PublicIP" {
-   value = [data.oci_core_vnic.FoggyKitchenBastionServer_VNIC1.public_ip_address]
+  value = [data.oci_core_vnic.FoggyKitchenBastionServer_VNIC1.public_ip_address]
 }
 
 output "FoggyKitchenPublicLoadBalancer_Public_IP" {
@@ -7,13 +7,14 @@ output "FoggyKitchenPublicLoadBalancer_Public_IP" {
 }
 
 output "FoggyKitchenWebserver1PrivateIP" {
-   value = [data.oci_core_vnic.FoggyKitchenWebserver1_VNIC1.private_ip_address]
+  value = [data.oci_core_vnic.FoggyKitchenWebserver1_VNIC1.private_ip_address]
 }
 
 output "FoggyKitchenWebserver2PrivateIP" {
-   value = [data.oci_core_vnic.FoggyKitchenWebserver2_VNIC1.private_ip_address]
+  value = [data.oci_core_vnic.FoggyKitchenWebserver2_VNIC1.private_ip_address]
 }
 
 output "generated_ssh_private_key" {
-  value = tls_private_key.public_private_key_pair.private_key_pem
+  value     = tls_private_key.public_private_key_pair.private_key_pem
+  sensitive = true
 }
--- LESSON9a_vcn_remote_peering_with_dataguard/output.tf
+++ LESSON9a_vcn_remote_peering_with_dataguard/output.tf
@@ -1,5 +1,5 @@
 output "FoggyKitchenBastionServer_PublicIP" {
-   value = [data.oci_core_vnic.FoggyKitchenBastionServer_VNIC1.public_ip_address]
+  value = [data.oci_core_vnic.FoggyKitchenBastionServer_VNIC1.public_ip_address]
 }
 
 output "FoggyKitchenPublicLoadBalancer_Public_IP" {
@@ -7,15 +7,15 @@ output "FoggyKitchenPublicLoadBalancer_Public_IP" {
 }
 
 output "FoggyKitchenWebserver1PrivateIP" {
-   value = [data.oci_core_vnic.FoggyKitchenWebserver1_VNIC1.private_ip_address]
+  value = [data.oci_core_vnic.FoggyKitchenWebserver1_VNIC1.private_ip_address]
 }
 
 output "FoggyKitchenWebserver2PrivateIP" {
-   value = [data.oci_core_vnic.FoggyKitchenWebserver2_VNIC1.private_ip_address]
+  value = [data.oci_core_vnic.FoggyKitchenWebserver2_VNIC1.private_ip_address]
 }
 
 output "FoggyKitchenDBServer_PrivateIP" {
-   value = [data.oci_core_vnic.FoggyKitchenDBSystem_VNIC1.private_ip_address]
+  value = [data.oci_core_vnic.FoggyKitchenDBSystem_VNIC1.private_ip_address]
 }
 
 output "FoggyKitchenBackendserver1_PrivateIP" {
@@ -23,5 +23,6 @@ output "FoggyKitchenBackendserver1_PrivateIP" {
 }
 
 output "generated_ssh_private_key" {
-  value = tls_private_key.public_private_key_pair.private_key_pem
+  value     = tls_private_key.public_private_key_pair.private_key_pem
+  sensitive = true
 }
