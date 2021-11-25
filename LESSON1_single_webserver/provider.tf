d5b6d0071eb552bafeebbfef369a7e31e3f9c1cb
mlinxfeld <martin.linxfeld@foggykitchen.com>
2021-11-25 13:02:50 +0100

    Fixes for LESSON1 and LESSON4

--- LESSON1_single_webserver/provider.tf
+++ LESSON1_single_webserver/provider.tf
@@ -1,19 +1,19 @@
 # General Provider 
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  #  user_ocid        = var.user_ocid
-  #  fingerprint      = var.fingerprint
-  #  private_key_path = var.private_key_path
-  region = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
 
 # Home Region Provider
 provider "oci" {
-  alias        = "homeregion"
-  tenancy_ocid = var.tenancy_ocid
-  #  user_ocid            = var.user_ocid
-  #  fingerprint          = var.fingerprint
-  #  private_key_path     = var.private_key_path
+  alias                = "homeregion"
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
   region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
   disable_auto_retries = "true"
 }
