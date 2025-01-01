987171a0c74d39f6d09b1739adbd3636277160bd
Martin Linxfeld <mlinxfeld@Martins-MacBook-Pro.local>
2025-01-01 13:55:30 +0100

    Updated LESSON1

--- LESSON1_single_webserver/variables.tf
+++ LESSON1_single_webserver/variables.tf
@@ -42,17 +42,3 @@ variable "service_ports" {
   default = [80, 443, 22]
 }
 
-# Dictionary Locals
-locals {
-  compute_flexible_shapes = [
-    "VM.Standard.E3.Flex",
-    "VM.Standard.E4.Flex",
-    "VM.Standard.A1.Flex",
-    "VM.Optimized3.Flex"
-  ]
-}
-
-# Checks if is using Flexible Compute Shapes
-locals {
-  is_flexible_shape = contains(local.compute_flexible_shapes, var.Shape)
-}
